4c1e350d254c69f93674c1e0ebde1fc30b78744d
Phuong A T <phuongt994@gmail.com>
2019-07-17 14:27:41 +0700

    redo week 1 assignments

--- week1/3-studentCount.js
+++ week1/3-studentCount.js
@@ -1,35 +1,32 @@
-const students = require("../common/students");
+const students = require('../commons/students');
 
 // using forEach()
 function countForEach() {
   let count = { male: 0, female: 0 };
 
   students.forEach(function(student) {
-    student.gender === "male" ? count.male++ : count.female++;
+    student.gender === 'male' ? count.male++ : count.female++;
   });
   console.log(
-    "Using forEach(), there are " +
-      count.male +
-      " male and " +
-      count.female +
-      " female students."
+    `Using forEach() there are ${count.male} male and ${
+      count.female
+    } female students.`
   );
 }
 
 // using reduce()
 function countReduce() {
-  const count = students.reduce(function(count, student) {
-    count[student.gender] = count[student.gender]
-      ? count[student.gender] + 1
-      : 1;
-    return count;
-  }, {});
+  const count = students.reduce(
+    function(count, student) {
+      count[student.gender] += 1;
+      return count;
+    },
+    { male: 0, female: 0 }
+  );
   console.log(
-    "Using reduce(), there are " +
-      count.male +
-      " male and " +
-      count.female +
-      " female students."
+    `Using reduce() there are ${count.male} male and ${
+      count.female
+    } female students.`
   );
 }
 
