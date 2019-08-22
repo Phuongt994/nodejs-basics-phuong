02f67ea46b64d4394e3bde7c71801f789a6bf0e9
Phuong A T <phuongt994@gmail.com>
2019-08-22 18:24:23 +0700

    re-setup skeleton app

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const adminRouter = require('./routes/admin');
-const productRouter = require('./routes/product');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const mongoose = require('mongoose');
-const {
-  // MONGO_URI = 'mongodb://localhost:27017/testdb',
-	MONGO_URI = 'mongodb+srv://admin:karakara13@cluster-nordiccoder-42e77.mongodb.net/test?retryWrites=true&w=majority'
-} = process.env;	
-
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log('connection to db successfully!');
-  });
-
-const app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
-app.use('/api', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
